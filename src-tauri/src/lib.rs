52acaa275075cdad331236a88bad6fed81b3f5f4
Yannis <52321382+yannis300307@users.noreply.github.com>
2024-12-26 17:56:05 +0100

    Adding tauri bindings

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,42 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use winreg::enums::*;
+use winreg::RegKey;
+use vdf_parser::*;
+use std::env;
+use std::fs;
+use std;
+use std::path::Path;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_steam_installed_apps() -> Vec<String> {
+    // Get the Steam path from the registry 
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let steam_registry_info = hklm.open_subkey("SOFTWARE\\WOW6432Node\\Valve\\Steam").expect("Can't find Steam in registry.");
+    let steam_path: String = steam_registry_info.get_value("InstallPath").expect("InstallPath is not readable.");
+
+    let library_folder_content = fs::read_to_string(Path::new(&steam_path).join("steamapps").join("libraryfolders.vdf"))
+        .expect("Can't access library folder file.");
+
+    let library_folder_parsed = vdf_parser::parse_vdf_text(&library_folder_content).expect("Error handling library folder VDF file");
+    
+    match library_folder_parsed.value {
+        vdf_parser::VdfValue::Block(block) => {
+            todo!("Iterate libraries")
+        }
+        vdf_parser::VdfValue::String(_) => {todo!("Handle error")}
+    }
+
+    let mut games: Vec<String> = Vec::new();
+
+    games.push("Hello".to_string());
+
+    games
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_steam_installed_apps])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-// When using the Tauri API npm package:
-//import { invoke } from '@tauri-apps/api/tauri'
-// When using the Tauri global script (if not using the npm package)
-// Be sure to set `build.withGlobalTauri` in `tauri.conf.json` to true
-//const invoke = window.__TAURI__.invoke
+const { invoke } = window.__TAURI__.core;
 
-// Invoke the command
-//invoke('my_custom_command')
+invoke('get_steam_installed_apps').then((message) => console.log(message));
 
 var last_card_id = 0;
 const card_delay = 150; // Milliseconds
