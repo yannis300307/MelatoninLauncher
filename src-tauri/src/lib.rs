5967589edfc0f639fee3a9f99ee770fad21885ee
yannis300307 <yannis300307@gmail.com>
2024-12-27 15:30:26 +0100

    Add steam path detection on linux

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,51 +1,81 @@
-use winreg::enums::*;
-use winreg::RegKey;
-use vdf_parser::*;
+use std;
 use std::env;
 use std::fs;
-use std;
 use std::path::Path;
+use winreg::enums::*;
+use winreg::RegKey;
+
+fn get_steam_path() -> String {
+    let steam_path = match env::consts::OS {
+        "windows" => {
+            let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+            let steam_registry_info = hklm
+                .open_subkey("SOFTWARE\\WOW6432Node\\Valve\\Steam")
+                .expect("Can't find Steam in registry.");
+            let steam_path: String = steam_registry_info
+                .get_value("InstallPath")
+                .expect("InstallPath is not readable.");
+            steam_path
+        }
+        "linux" => {
+            if Path::exists(Path::new("~/.local/share/Steam")) {
+                let steam_path = "~/.local/share/Steam".to_string();
+                steam_path
+            } else {
+                todo!("Mettre une erreur");
+            }
+        }
+        _ => {
+            panic!("Ce systeme d'exploitation n'est pas supporté.");
+        }
+    };
+
+    steam_path
+}
 
 #[tauri::command]
 fn get_steam_installed_apps() -> Vec<String> {
-    // Get the Steam path from the registry 
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let steam_registry_info = hklm.open_subkey("SOFTWARE\\WOW6432Node\\Valve\\Steam").expect("Can't find Steam in registry.");
-    let steam_path: String = steam_registry_info.get_value("InstallPath").expect("InstallPath is not readable.");
+    // Get the Steam path from the registry
+    let steam_path = get_steam_path();
 
-    let library_folder_content = fs::read_to_string(Path::new(&steam_path).join("steamapps").join("libraryfolders.vdf"))
-        .expect("Can't access library folder file.");
+    let library_folder_content = fs::read_to_string(
+        Path::new(&steam_path)
+            .join("steamapps")
+            .join("libraryfolders.vdf"),
+    )
+    .expect("Can't access library folder file.");
 
-    let library_folder_parsed = vdf_parser::parse_vdf_text(&library_folder_content).expect("Error handling library folder VDF file.");
-    
+    let library_folder_parsed = vdf_parser::parse_vdf_text(&library_folder_content)
+        .expect("Error handling library folder VDF file.");
+
     let mut games: Vec<String> = Vec::new();
 
     match library_folder_parsed.value {
         vdf_parser::VdfValue::Block(library_folder_block) => {
             for library in library_folder_block.values() {
                 match &library.value {
-                    vdf_parser::VdfValue::Block(library_block) => {
-                        match library_block.get("apps") {
-                            Some(app) => {
-                                match &app.value {
-                                    vdf_parser::VdfValue::Block(game_id) => {
-                                        for game in game_id.keys() {
-                                            games.push(game.to_string());
-                                        }
-                                    }
-
-                                    vdf_parser::VdfValue::String(_) => {panic!("Library VDF file is invalid.");}
+                    vdf_parser::VdfValue::Block(library_block) => match library_block.get("apps") {
+                        Some(app) => match &app.value {
+                            vdf_parser::VdfValue::Block(game_id) => {
+                                for game in game_id.keys() {
+                                    games.push(game.to_string());
                                 }
-                                
-                            },
-                            None => panic!("Library VDF file is invalid.")
-                        }
+                            }
+                            vdf_parser::VdfValue::String(_) => {
+                                panic!("Library VDF file is invalid.");
+                            }
+                        },
+                        None => panic!("Library VDF file is invalid."),
+                    },
+                    vdf_parser::VdfValue::String(_) => {
+                        panic!("Library VDF file is invalid.");
                     }
-                    vdf_parser::VdfValue::String(_) => {panic!("Library VDF file is invalid.");}
                 }
             }
         }
-        vdf_parser::VdfValue::String(_) => {panic!("Library VDF file is invalid.");}
+        vdf_parser::VdfValue::String(_) => {
+            panic!("Library VDF file is invalid.");
+        }
     }
 
     games
