c321e2e81e3d756267212ea7b4bba64ed9f085bb
Yannis <52321382+yannis300307@users.noreply.github.com>
2024-12-31 18:03:57 +0100

    Add game reload

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ const page_games_list = document.getElementById("page-games-list");
 const steam_scan_button = document.getElementById("steam-scan-button");
 const base_game_found_list_element = document.getElementById("base-game-found-list-element");
 const steam_scan_result_list = document.getElementById("steam-scan-result-list");
+const notif_container = document.getElementById("notif-container");
 
 const TM_BASE_URL = "https://team-melatonin.fr/";
 
@@ -44,6 +45,17 @@ function add_game(name, image) {
   last_card_id++;
 }
 
+function show_notif(text, delay) {
+  notif_container.innerText = text;
+  notif_container.style.transform = "translateY(calc(100vh - " + Math.max(notif_container.getBoundingClientRect().height + 20) + "px))";
+  notif_container.style.opacity = "1";
+  console.log("translateY(100vh - " + Math.max(notif_container.getBoundingClientRect().height) + "px)");
+  setTimeout(() => {
+    notif_container.style.transform = "";
+    notif_container.style.opacity = "";
+  }, delay);
+}
+
 function dispawn_game_page() {
   let cards = document.getElementsByClassName("game-card");
 
@@ -51,7 +63,7 @@ function dispawn_game_page() {
     cards[i].classList = "game-card game-card-dispawned";
   }
 
-  document.getElementById("add-game-button").style.animation = "dispawn-add-game-button 1s ease 0.2s forwards";
+  document.getElementById("reload-games-button").style.animation = "dispawn-reload-games-button 1s ease 0.2s forwards";
 
   setTimeout(() => { page_games_list.style.display = "none"; }, 1000);
 }
@@ -60,7 +72,7 @@ function steam_scan_clicked() {
   if (steam_scan_loading) return;
 
   steam_scan_loading = true;
-  
+
   steam_scan_button.classList = "button button-loading add-game-buttons";
   steam_scan_button.style.animation = "button-to-loading 1s ease forwards, rotate 5s infinite 500ms linear";
 
@@ -70,17 +82,17 @@ function steam_scan_clicked() {
       for (let i = 0; i < message.length; i++) {
         let current_app = message[i];
         if (found_apps.includes(current_app["global_id"])) continue;
-      
+
         found_apps.push(current_app["global_id"]);
 
         let element_copy = base_game_found_list_element.cloneNode(true);
         element_copy.id = "";
-        element_copy.style.animationDelay = i*200 + "ms";
+        element_copy.style.animationDelay = i * 200 + "ms";
         element_copy.getElementsByClassName("add-game-found-list-element-name")[0].innerText = current_app["name"];
         element_copy.getElementsByTagName("img")[0].src = TM_BASE_URL + current_app["icon"];
         steam_scan_result_list.appendChild(element_copy);
       }
-      
+
       steam_scan_button.style.animation = "button-to-loading-reversed 1s ease forwards";
       steam_scan_button.classList = "button add-game-buttons";
       steam_scan_loading = false;
@@ -88,14 +100,38 @@ function steam_scan_clicked() {
   }, 1000)
 }
 
+function reload_game_cards(first_load=false) {
+  let cards = document.querySelectorAll(".game-card:not(#base_card):not(#base_card_fake)");
+
+  if (!first_load) document.getElementById("reload-games-button").style.animation = "dispawn-reload-games-button 1s ease 0.2s forwards";
+
+  for (let i = 0; i < cards.length; i++) {
+    let current_card = cards[i];
+    current_card.classList = "game-card game-card-dispawned";
+  }
+
+  setTimeout(() => {
+    for (let i = 0; i < cards.length; i++) {
+      page_games_list.removeChild(cards[i]);
+    }
+
+    invoke("get_remote_available_patches").then((message) => {
+      console.log(message);
+      for (let i = 0; i < message.length; i++) {
+        add_game(message[i]["name"], TM_BASE_URL + message[i]["card_image"]);
+      }
+    }).catch((error) => {
+      show_notif("Impossible de récupérer la liste des patchs. Veuillez vérifier votre connexion à Internet. Erreur du backend : " + error, 15000);
+    });
+
+    document.getElementById("reload-games-button").style.animation = "";
+  }, 1000);
+
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("add-game-button").addEventListener("click", dispawn_game_page);
+  document.getElementById("reload-games-button").addEventListener("click", () => {reload_game_cards(false)});
   document.getElementById("steam-scan-button").addEventListener("click", steam_scan_clicked);
 
-  invoke("get_remote_available_patches").then((message) => {
-    console.log(message);
-    for (let i=0; i<message.length; i++) {
-      add_game(message[i]["name"], TM_BASE_URL+message[i]["card_image"]);
-    }
-  });
+  reload_game_cards(true);
 });
